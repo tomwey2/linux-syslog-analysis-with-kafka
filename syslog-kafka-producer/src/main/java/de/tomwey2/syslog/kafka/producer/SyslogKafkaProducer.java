5374dd5887838b7b945b26788525617e4798c9eb
Thomas <tomwey2@gmail.com>
2023-10-24 09:51:04 +0400

    updated producer

--- syslog-kafka-producer/src/main/java/de/tomwey2/syslog/kafka/producer/SyslogKafkaProducer.java
+++ syslog-kafka-producer/src/main/java/de/tomwey2/syslog/kafka/producer/SyslogKafkaProducer.java
@@ -2,10 +2,6 @@ package de.tomwey2.syslog.kafka.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-
-import java.beans.ConstructorProperties;
 
 @SpringBootApplication
 public class SyslogKafkaProducer {
