91ca9d59aadd879b2dba3071304f0fad1702193c
Thomas <tomwey2@gmail.com>
2023-10-25 07:38:59 +0400

    implemented consumer

--- syslog-kafka-consumer/src/main/java/de/tomwey2/syslog/kafka/consumer/SyslogKafkaConsumer.java
+++ syslog-kafka-consumer/src/main/java/de/tomwey2/syslog/kafka/consumer/SyslogKafkaConsumer.java
@@ -1,12 +1,62 @@
 package de.tomwey2.syslog.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SyslogKafkaConsumer {
+    @Value(value = "${spring.kafka.bootstrap.servers}")
+    private String bootstrapServers;
+
+    @Value(value = "${spring.kafka.application.id}")
+    private String applicationId;
+
+    @Value(value = "${spring.kafka.group.id}")
+    private String groupId;
+
     public static void main(String[] args) {
         SpringApplication.run(SyslogKafkaConsumer.class, args);
     }
 
+    @KafkaListener(id = "${spring.kafka.application.id}",
+            topics = "#{'${spring.kafka.input.topics}'.split(',')}")
+    public void listen(String in) {
+        System.out.println(in);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<Integer, String>
+    kafkaListenerContainerFactory(ConsumerFactory<Integer, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<Integer, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerProps());
+    }
+
+    private Map<String, Object> consumerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        // ...
+        return props;
+    }
 }
