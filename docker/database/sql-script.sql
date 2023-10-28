da88aa33906ace3a2f843fe376a445891622ed98
Thomas <tomwey2@gmail.com>
2023-10-28 11:40:10 +0400

    updated docker-compose.yml and db script for the database

---
+++ docker/database/sql-script.sql
@@ -0,0 +1,33 @@
+create database if not exists 'syslogdb';
+create user user identified by 'user';
+grant all privileges on syslogdb.* to user;
+show grants for user;
+
+alter database 'syslogdb' character set utf8 collate utf8_unicode_ci;
+use 'syslogdb';
+
+drop table if exists syslogs;
+create table syslogs (
+    id integer not null auto_increment,
+    filename varchar(25),
+    primary key (id)
+);
+
+drop table if exists failed_logins;
+create table failed_logins (
+    id integer not null auto_increment,
+    syslogs_id integer,
+    logtime timestamp not null,
+    message varchar(50),
+    host varchar(25),
+    user varchar(25),
+    failure_count integer,
+    primary key (id)
+);
+
+drop constraint failed_logins_syslogs
+alter table failed_logins
+    add constraint failed_logins_syslogs
+        foreign key (syslogs_id)
+            references syslogs (id)
+
